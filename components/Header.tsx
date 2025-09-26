b7c4c838338ca89c84246f29c91fc68cc9aa8a6f
alvitaaz <alvita.azalia@student.ac.id>
2025-09-26 21:04:09 +0700

    Initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { Session } from "next-auth"; 
+
+type HeaderProps = {
+  session?: Session | null;
+};
+
+const Header = ({ session }: HeaderProps) => {
+  return (
+    <header className="my-10 flex justify-between gap-5">
+      <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
+
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <p className="text-sm font-medium">
+            {session?.user?.name ?? "Pengguna"}
+          </p>
+        </li>
+        <li>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+            className="mb-10"
+          >
+            <Button>Logout</Button>
+          </form>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
